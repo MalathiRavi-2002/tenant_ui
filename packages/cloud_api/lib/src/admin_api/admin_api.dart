d06422e26e74b00b10a213ec85d37f803048574f
leefordjudes <leefordjudes@gmail.com>
2025-06-11 10:51:38 +0530

    org init - gst no added

--- packages/cloud_api/lib/src/admin_api/admin_api.dart
+++ packages/cloud_api/lib/src/admin_api/admin_api.dart
@@ -13,22 +13,19 @@ class Api extends ApiBase {
     }).map((e) => AdminLoginResponse.fromJson(e as Map<String, dynamic>));
   }
 
-  AsyncResult<List<OrganizationResponseModel>> organizationList() {
-    return get('/list_organization', {}).map((e) {
-      final list = e as List<dynamic>;
-      print(list);
-      return list.map((item) => OrganizationResponseModel.fromJson(item)).toList();
+  AsyncResult<List<String>> organizationList() {
+    return get('/list_organization', {}).map((res) {
+      final list = res as List<dynamic>;
+      return list.map((el) => el).toList().cast<String>();
+      // return list.map((item) => OrganizationResponseModel.fromJson(item)).toList();
     });
   }
 
   AsyncResult<OrganizationCreateResponse> createOrganization(OrganizationCreateRequest req) {
-    return post('/organization_init', {
-      'name': req.name,
-      'full_name': req.fullName,
-      'country': req.country,
-      'book_begin': req.bookBegin,
-      'fp_code': req.fpCode,
-    }).map((e) => OrganizationCreateResponse.fromJson(e as Map<String, dynamic>));
+    return post(
+      '/organization_init',
+      req.toJson(),
+    ).map((e) => OrganizationCreateResponse.fromJson(e as Map<String, dynamic>));
   }
 
   AsyncResult<RegisterResponse> register(RegisterRequest data) {
